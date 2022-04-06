1d8d6664e76e6907b2a07ad57e5c2df23009a52c
Kmek <kmekeefe@gmail.com>
2022-04-05 21:30:58 -0400

    List dining halls on stats webpage with links

--- webpage/public/scripts/bot-stats.js
+++ webpage/public/scripts/bot-stats.js
@@ -1,9 +1,17 @@
 // Script for the main page of the bot stats webpage
 
-//******************** Fill tablesList ********************//
+//******************** Fill Tables List ********************//
 const tablesList = document.getElementById("tablesList");
 
 // Populate the unordered list with a list of models (once)
 getModels()
     .then(models => models
         .forEach(table => tablesList.innerHTML += "<li>" + table + "</li>"));
+
+//******************** Fill Dining Halls List ********************//
+const diningHallsList = document.getElementById("diningHallsList");
+
+// Populates the list with the names of the dining halls
+getTable("DiningHalls")
+    .then(halls => halls
+        .forEach(hall => diningHallsList.innerHTML += "<li><a href='" + hall.url + "' target='_blank'>" + hall.name + "</a></li>"));
